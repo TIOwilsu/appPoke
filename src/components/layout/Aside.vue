3b0434d4cffaf806b5b975950de33a88f5cc6891
tiowilsu <wilson.queiroz@outlook.com>
2021-08-31 01:06:27 -0300

    create home page

---
+++ src/components/layout/Aside.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-navigation-drawer :value="getAside" absolute top temporary @input="input">
+    <v-list nav dense>
+      <v-list-item-group
+        v-model="group"
+        active-class="deep-blue--text text--accent-4"
+      >
+        <v-list-item>
+          <v-list-item-title>Foo</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-title>Bar</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-title>Fizz</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-title>Buzz</v-list-item-title>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script>
+import { mapGetters, mapMutations } from "vuex";
+export default {
+  name: "Aside",
+  data: () => ({
+    show: false,
+    group: null
+  }),
+  created() {
+    this.show = this.getAside;
+  },
+  computed: {
+    ...mapGetters(["getAside"])
+  },
+  methods: {
+    ...mapMutations(["toogleAside"]),
+    input(value) {
+      if (!value) this.toogleAside(!value);
+    }
+  },
+  watch: {
+    group() {
+      this.toogleAside(this.getAside);
+    }
+  }
+};
+</script>
